2452c99ed6d7750e2b2f0a54c085a7416e8b0073
Srinivas K M <150222784+Srinivas1899@users.noreply.github.com>
2023-12-18 10:45:21 +0530

    Create  Shuffle Integers.py
    Shuffle integers
    
    Given an array arr of n elements in the following format {a1, a2, a3, a4, ... , an/2, b1, b2, b3, b4, ... , bn/2}, the task is shuffle the array to {a1, b1, a2, b2, a3, b3, ... , an/2, bn/2} without using extra space.
    Note that n is even.
    
    Example 1:
    
    Input: 
    n = 4, arr = {1, 2, 9, 15}
    Output:  
    1 9 2 15
    Explanation: 
    a1=1, a2=2, b1=9, b2=15. So the final array will be: a1, b1, a2, b2 = {1,9,2,15}.
    Example 2:
    
    Input: 
    n = 6 arr = {1, 2, 3, 4, 5, 6} 
    Output: 
    1 4 2 5 3 6
    Your Task:
    This is a function problem. You don't need to take any input, as it is already accomplished by the driver code. You just need to complete the function shuffleArray() that takes array arr, and an integer n as parameters and modifies the given array according to the above-given pattern.
    
    Expected Time Complexity: O(n).
    Expected Auxiliary Space: O(1).
    
    Constraints:
    1 ≤ n ≤ 105
    1 ≤ arri ≤ 103

---
+++ Codes/Shuffle Integers (25-11-2023)/Shuffle Integers.py
@@ -0,0 +1,9 @@
+class Solution:
+    def shuffleArray(self, arr, n):
+        # Your code goes here
+        m = n//2
+        for i in range(m):
+            arr[i + m] += arr[i]*1001
+        for i in range(m):
+            arr[2*i] = arr[i + m]//1001
+            arr[2*i + 1] = arr[i + m]%1001
